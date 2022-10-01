18af431d662964b62cbc88d8714bce72ce01576b
Mohamed Amine Dahmen <med-amine.dahmen@proxym-it.com>
2022-10-01 12:17:04 +0100

    initialize project

---
+++ src/test/java/com/user/jitpay/user/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.jitpay.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
